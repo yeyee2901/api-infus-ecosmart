ad6029209a6d35fb5dd8c139c143fbd0e5d8591d
yeyee2901 <gabriel777sh@gmail.com>
2021-11-29 22:10:56 +0700

    FORGOT TO UNCOMMENT THINGS

--- src/routes/infus.ts
+++ src/routes/infus.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import {
   queryData,
-  IInfusData as _InfusData
+  IInfusData
 } from '../controller/controller_infus';
 
 const router = express.Router();
@@ -16,24 +16,22 @@ const query = `SELECT * FROM ${table}`;
 
 router.route('/infus/:id').get((_request, response) => {
   queryData(host, user, password, database, query, result_array => {
-    console.log(result_array);
-    response.send(`Helloo`);
-    // // check if result is array of records
-    // if( Array.isArray(result_array) ){
-    //   const result = result_array[0]
+    // check if result is array of records
+    if( Array.isArray(result_array) ){
+      const result = result_array[0]
 
-    //   // assert the type
-    //   if( (result as IInfusData).id ){
-    //     const dataInfus = result as IInfusData
-    //     console.log(dataInfus);
-    //     dataInfus.id = 1
-    //     dataInfus.stringBlob = `hello`
-    //     console.log(dataInfus);
-    //     response.json({ id: dataInfus.id, stringBlob: dataInfus.stringBlob })
-    //   }
-    // }else{
-    //   response.json({ msg: 'No Data!' }).status(401)
-    // }
+      // assert the type
+      if( (result as IInfusData).id ){
+        const dataInfus = result as IInfusData
+        console.log(dataInfus);
+        dataInfus.id = 1
+        dataInfus.stringBlob = `hello`
+        console.log(dataInfus);
+        response.json({ id: dataInfus.id, stringBlob: dataInfus.stringBlob })
+      }
+    }else{
+      response.json({ msg: 'No Data!' }).status(401)
+    }
   });
 });
 
