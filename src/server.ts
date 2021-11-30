cff84aa70ff0f224d26c8b9e71f0d344ba98fd70
yeyee2901 <gabriel777sh@gmail.com>
2021-11-30 10:07:30 +0700

    ADD: dotenv for environmental config

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
-import express from 'express'
-import notFound from './routes/notFound'
-import infus from './routes/infus'
+import 'dotenv/config'
+import express from 'express';
+import notFound from './routes/notFound';
+import InfusRoutes from './routes/infus';
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = parseInt(process.env.SERVER_PORT as string) || 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (_req, res) => {
-  res.send('Hellooo')
-})
+  res.send('Hellooo');
+});
 
-app.use('/api', infus)
-app.use('*', notFound)
+app.use('/api/infus', InfusRoutes);
+app.use('*', notFound);
 app.listen(PORT, () => {
   console.log(`Server on http://localhost:${PORT}`);
-})
+});
